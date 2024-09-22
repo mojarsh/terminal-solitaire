1b81505d9ca1aadb0282fe4683dd515bee850289
Joe Marshall <joemarshall97@live.co.uk>
2024-09-22 18:57:15 +0100

    Updated unit tests for Deck module following refactor

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,13 +1,31 @@
-from terminal_solitaire.deck import Deck
+from terminal_solitaire.deck import Card, Deck, build_deck, shuffle_deck
 
-VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-SUITS = ["Spades", "Clubs", "Hearts", "Diamonds"]
-deck = Deck(VALUES, SUITS)
+deck = build_deck()
+shuffled = shuffle_deck(deck)
 
 
 def test_deck_class_instance() -> None:
     assert isinstance(deck, Deck)
 
 
+def test_card_class_instance() -> None:
+    assert isinstance(deck.cards[0], Card)
+
+
+def test_card_display_false_value() -> None:
+    card = Card("\u2660", "A", "Black")
+    assert card.display_value == 2 * "\u2587"
+
+
+def test_card_display_true_value() -> None:
+    card = Card("\u2660", "A", "Black", True)
+    assert card.display_value == "A\u2660"
+
+
 def test_deck_is_correct_size() -> None:
-    assert len(deck.build()) == 52
+    deck = build_deck()
+    assert len(deck.cards) == 52
+
+
+def test_shuffled_deck_is_different() -> None:
+    assert deck is not shuffled
