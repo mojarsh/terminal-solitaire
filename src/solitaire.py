698b30f89d8403f32acc87b03bb93ce293c2f856
Joe Marshall <joemarshall97@live.co.uk>
2024-09-24 19:16:51 +0100

    Updated so that user only has to enter column number to move cards

--- src/solitaire.py
+++ src/solitaire.py
@@ -1,13 +1,9 @@
-from pprint import pprint
-
 from terminal_solitaire.deck import build_deck, shuffle_deck
-from terminal_solitaire.game_board import Board, draw_board, place_cards_on_tableau
-from terminal_solitaire.tableau import (
-    generate_original_tableau,
-    place_card_on_board,
-    remove_card_on_board,
-    reveal_card_on_board,
-    select_card_on_board,
+from terminal_solitaire.game_board import (
+    Board,
+    draw_board,
+    generate_board_element,
+    place_cards_on_tableau,
 )
 
 
@@ -15,40 +11,24 @@ def main() -> None:
 
     deck = build_deck()
     shuffled = shuffle_deck(deck)
-    board = Board(13, 7)
-    tableau = board.generate_tableau_board()
-    foundations = board.generate_foundations_board()
-    dealt_cards = generate_original_tableau(shuffled, board)
-    place_cards_on_tableau(dealt_cards, tableau)
-    draw_board(foundations, tableau)
+    tableau = generate_board_element(13, 7)
+    foundations = generate_board_element(1, 7)
+    board = Board(foundations, tableau)
+    dealt_cards = board.deal_initial_tableau(shuffled)
+    place_cards_on_tableau(dealt_cards, tableau.board)
+    draw_board(board)
     while True:
         try:
-            move_from = input("\nEnter the coordinates of the card to move (R, C): ")
-            move_to = input("Enter the destination coordinates (R, C): ")
-            move_from_coordinates = tuple(int(x) for x in move_from.split(","))
-            move_to_coordinates = tuple(int(x) for x in move_to.split(","))
-            selected_card = select_card_on_board(
-                tableau,
-                row_index=move_from_coordinates[0],
-                column_index=move_from_coordinates[1],
-            )
-            remove_card_on_board(
-                tableau,
-                row_index=move_from_coordinates[0],
-                column_index=move_from_coordinates[1],
-            )
-            place_card_on_board(
-                card=selected_card,
-                tableau=tableau,
-                row_index=move_to_coordinates[0],
-                column_index=move_to_coordinates[1],
-            )
-            reveal_card_on_board(
-                tableau,
-                row_index=move_from_coordinates[0],
-                column_index=move_from_coordinates[1],
-            )
-            draw_board(foundations, tableau)
+            move_from = int(input("\nEnter the column of the card to move: "))
+            move_to = int(input("Enter the destination column: "))
+            from_coordinates = board.find_last_card_in_tableau_column(move_from)
+            to_coordinates = board.find_next_free_space_in_tableau_column(move_to)
+            selected_card = board.select_card_on_tableau(from_coordinates)
+            board.remove_card_on_tableau(from_coordinates)
+            board.place_card_on_tableau(card=selected_card, coordinates=to_coordinates)
+            reveal_coordinates = board.find_last_card_in_tableau_column(move_from)
+            board.reveal_card_on_tableau(reveal_coordinates)
+            draw_board(board)
 
         except EOFError:
             break
--- src/terminal_solitaire/game_board.py
+++ src/terminal_solitaire/game_board.py
@@ -1,49 +1,80 @@
 import itertools
 from dataclasses import dataclass
 
-from terminal_solitaire.deck import Card
+from terminal_solitaire.deck import Card, Deck
+
+
+@dataclass
+class BoardElement:
+    board: dict[tuple : str | Card]
+    rows: int
+    columns: int
+
+
+def generate_board_element(rows: int, columns: int) -> BoardElement:
+    element_rows = [_ for _ in range(rows + 1)]
+    element_columns = [_ for _ in range(columns)]
+
+    board = {k: "  " for k in tuple(itertools.product(element_rows, element_columns))}
+    return BoardElement(board, rows, columns)
 
 
 @dataclass
 class Board:
-    rows: int
-    columns: int
+    foundations: BoardElement
+    tableau: BoardElement
 
-    def generate_tableau_board(self) -> dict[str | Card]:
-        tableau_rows = [_ for _ in range(self.rows + 1)]
-        tableau_columns = [_ for _ in range(self.columns)]
+    def deal_initial_tableau(self, deck: Deck) -> list[list[Card]]:
+        return [
+            deck.deal(number_of_cards=self.tableau.columns - i)
+            for i in range(self.tableau.columns)
+        ]
 
-        return {
-            k: "  " for k in tuple(itertools.product(tableau_rows, tableau_columns))
-        }
+    def find_last_card_in_tableau_column(self, column_index: int) -> tuple[int, int]:
+        row_indices = [
+            k[0]
+            for k, v in self.tableau.board.items()
+            if k[1] == column_index and isinstance(v, Card)
+        ]
+        last_row_index = max(row_indices)
 
-    def generate_foundations_board(self) -> dict[str | Card]:
-        foundation_rows = [_ for _ in range(1)]
-        foundation_columns = [_ for _ in range(self.columns)]
+        return (last_row_index, column_index)
 
-        return {
-            k: "  "
-            for k in tuple(itertools.product(foundation_rows, foundation_columns))
-        }
+    def find_next_free_space_in_tableau_column(
+        self, column_index: int
+    ) -> tuple[int, int]:
+        last_card = self.find_last_card_in_tableau_column(column_index)
+        return (last_card[0] + 1, column_index)
+
+    def select_card_on_tableau(self, coordinates: tuple[int, int]) -> Card:
+        return self.tableau.board[coordinates]
+
+    def remove_card_on_tableau(self, coordinates: tuple[int, int]) -> None:
+        self.tableau.board[coordinates] = "  "
+
+    def place_card_on_tableau(self, card: Card, coordinates: tuple[int, int]) -> None:
+        self.tableau.board[coordinates] = card
+
+    def reveal_card_on_tableau(self, coordinates: tuple[int, int]) -> None:
+        card = self.tableau.board[coordinates]
+        card.display_status = True
 
 
-def draw_board(
-    foundations_board: dict[tuple : str | Card], tableau_board: dict[tuple : str | Card]
-) -> None:
+def draw_board(board: Board) -> None:
     print("\n    00 01 02 03 04 05 06  ")
     print("  + -------------------- +")
-    for i in range(1):
+    for i in range(board.foundations.rows):
         print("  |", end=" ")
-        for k, v in foundations_board.items():
+        for k, v in board.foundations.board.items():
             if k[0] == i and isinstance(v, Card):
                 print(v.display_value, end=" ")
             elif k[0] == i and isinstance(v, str):
                 print(v, end=" ")
         print("|\r")
     print("  + -------------------- +")
-    for i in range(14):
+    for i in range(board.tableau.rows + 1):
         print(f"{i:02}|", end=" ")
-        for k, v in tableau_board.items():
+        for k, v in board.tableau.board.items():
             if k[0] == i and isinstance(v, Card):
                 print(v.display_value, end=" ")
             elif k[0] == i and isinstance(v, str):
@@ -53,7 +84,7 @@ def draw_board(
 
 
 def place_cards_on_tableau(
-    dealt_cards: list[list[Card]], tableau: dict[str | Card]
+    dealt_cards: list[list[Card]], tableau: BoardElement
 ) -> None:
     for row in dealt_cards:
         row_index = dealt_cards.index(row)
