3716abb6ebcbc38044ec849df2878be05f6f1cf6
Joe Marshall <joemarshall97@live.co.uk>
2026-03-16 21:09:56 +0000

    Cleaned up existing tests and added more for coverage

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,72 +1,74 @@
+import pytest
 from terminal_solitaire.board import Board, Tableau, generate_tableau
 from terminal_solitaire.deck import Card, build_deck
 
-tableau = generate_tableau(13, 7)
 
 
-def test_generated_board_is_correct_size() -> None:
+@pytest.fixture
+def tableau() -> Tableau:
+    return generate_tableau(13, 7)
+
+@pytest.fixture
+def dealt_tableau() -> Tableau:
+    t = generate_tableau(13, 7)
+    t.deal_initial_tableau(build_deck())
+
+    return t
+
+def test_generated_board_is_correct_size(tableau: Tableau) -> None:
     assert tableau.rows == 13 and tableau.columns == 7
 
 
-def test_board_instance() -> None:
+def test_board_instance(tableau: Tableau) -> None:
     assert isinstance(tableau, Tableau)
 
 
-def test_stack_of_revealed_cards() -> None:
-    deck = build_deck()
-    tableau.deal_initial_tableau(deck)
-    revealed_cards = tableau.get_stack_of_revealed_cards(1)
+def test_stack_of_revealed_cards(dealt_tableau: Tableau) -> None:
+    revealed_cards = dealt_tableau.get_stack_of_revealed_cards(1)
     assert len(revealed_cards.keys()) == 1
 
 
-def test_number_of_cards_dealt_onto_board() -> None:
-    deck = build_deck()
-    tableau.deal_initial_tableau(deck)
-    dealt_cards = [value for _, _, value in tableau if value != "  "]
+def test_number_of_cards_dealt_onto_board(dealt_tableau: Tableau) -> None:
+    dealt_cards = [value for _, _, value in dealt_tableau if value != "  "]
     assert len(dealt_cards) == 28
 
 
-def test_status_of_cards_dealt_onto_board() -> None:
-    deck = build_deck()
-    tableau.deal_initial_tableau(deck)
-    dealt_cards = [value for _, _, value in tableau if value != "  "]
-    hidden_cards = [card for card in dealt_cards if card.display_status == False]
-    shown_cards = [card for card in dealt_cards if card.display_status == True]
+def test_status_of_cards_dealt_onto_board(dealt_tableau: Tableau) -> None:
+    dealt_cards = [value for _, _, value in dealt_tableau if isinstance(value, Card)]
+    hidden_cards = [card for card in dealt_cards if not card.display_status]
+    shown_cards = [card for card in dealt_cards if card.display_status]
     assert len(hidden_cards) == 21 and len(shown_cards) == 7
 
 
-def test_find_coordinates_of_last_card_happy_path() -> None:
-    deck = build_deck()
-    tableau.deal_initial_tableau(deck)
-    assert tableau.find_coordinates_of_last_card(6) == (6, 6)
+def test_find_coordinates_of_last_card_happy_path(dealt_tableau: Tableau) -> None:
+    assert dealt_tableau.find_coordinates_of_last_card(6) == (6, 6)
 
 
-def test_find_coordinates_of_last_card_unhappy_path() -> None:
-    empty_tableau = generate_tableau(13, 7)
-    assert empty_tableau.find_coordinates_of_last_card(6) == None
+def test_find_coordinates_of_last_card_unhappy_path(tableau: Tableau) -> None:
+    assert tableau.find_coordinates_of_last_card(6) is None
 
 
-def test_find_coordinates_of_next_space() -> None:
-    assert tableau.find_coordinates_of_next_space(6) == (7, 6)
+def test_find_coordinates_of_next_space(dealt_tableau: Tableau) -> None:
+    assert dealt_tableau.find_coordinates_of_next_space(6) == (7, 6)
 
 
-def test_select_card_on_board() -> None:
-    assert isinstance(tableau.select_card_on_board((0, 0)), Card)
+def test_select_card_on_board(dealt_tableau: Tableau) -> None:
+    assert isinstance(dealt_tableau.select_card_on_board((0, 0)), Card)
 
 
-def test_remove_card_from_board() -> None:
-    tableau.remove_card_from_board((0, 0))
+def test_remove_card_from_board(dealt_tableau: Tableau) -> None:
+    dealt_tableau.remove_card_from_board((0, 0))
 
-    assert tableau.board[(0, 0)] == "  "
+    assert dealt_tableau.board[(0, 0)] == "  "
 
 
-def test_place_card_on_board() -> None:
+def test_place_card_on_board(tableau: Tableau) -> None:
     card = Card("test", "test", "test")
     tableau.place_card_on_board(card, (0, 0))
     assert tableau.board[(0, 0)] == card
 
 
-def test_reveal_card_on_board() -> None:
-    tableau.reveal_card_on_board((0, 6))
-
-    assert tableau.board[(0, 6)].display_status == True
+def test_reveal_card_on_board(dealt_tableau) -> None:
+    dealt_tableau.reveal_card_on_board((0, 6))
+    if isinstance(dealt_tableau.select_card_on_board((0, 6)), Card):
+        assert dealt_tableau.board[(0, 6)].display_status is True
--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,3 +1,4 @@
+import pytest
 from terminal_solitaire.deck import (
     Card,
     Deck,
@@ -7,15 +8,19 @@ from terminal_solitaire.deck import (
     shuffle_deck,
 )
 
-deck = build_deck()
-shuffled = shuffle_deck(deck)
+@pytest.fixture
+def deck() -> Deck:
+    return build_deck()
 
+@pytest.fixture
+def shuffled(deck: Deck) -> Deck:
+    return shuffle_deck(deck)
 
-def test_deck_class_instance() -> None:
+def test_deck_class_instance(deck: Deck) -> None:
     assert isinstance(deck, Deck)
 
 
-def test_card_class_instance() -> None:
+def test_card_class_instance(deck: Deck) -> None:
     assert isinstance(deck.cards[0], Card)
 
 
@@ -29,16 +34,14 @@ def test_card_display_true_value() -> None:
     assert card.display_value == "A\u2660"
 
 
-def test_deck_is_correct_size() -> None:
-    deck = build_deck()
+def test_deck_is_correct_size(deck: Deck) -> None:
     assert len(deck.cards) == 52
 
 
-def test_shuffled_deck_is_different() -> None:
-    assert deck is not shuffled
+def test_shuffled_deck_is_different(deck: Deck, shuffled: Deck) -> None:
+    assert [c.value + c.suit for c in deck.cards] != [c.value + c.suit for c in shuffled.cards]
 
-
-def test_number_of_dealt_cards() -> None:
+def test_number_of_dealt_cards(shuffled: Deck) -> None:
     deal = shuffled.deal(7)
     assert len(deal) == 7
 
